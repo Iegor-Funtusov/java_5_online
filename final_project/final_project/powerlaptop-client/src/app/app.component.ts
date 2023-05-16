7c5f48fbb7e8f2be2529873514929b0fa45647a8
egorfuntusov <funtushan@gmail.com>
2023-05-16 21:16:54 +0300

    add angular view

---
+++ final_project/final_project/powerlaptop-client/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {RouterOutlet} from "@angular/router";
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  imports: [
+    RouterOutlet
+  ],
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'powerlaptop-client';
+}
---
+++ final_project/final_project/powerlaptop-client/src/app/pages/plp/plp.component.ts
@@ -0,0 +1,27 @@
+import {Component, OnInit} from '@angular/core';
+import {PlpService} from "../../services/plp.service";
+import {DataContainer} from "../../models/data.container";
+import {ProductPlpModel} from "../../models/product-plp.model";
+import {AsyncPipe, JsonPipe, NgForOf, NgIf} from "@angular/common";
+import {Observable} from "rxjs";
+
+@Component({
+  selector: 'app-plp',
+  standalone: true,
+  templateUrl: './plp.component.html',
+  imports: [
+    JsonPipe,
+    AsyncPipe,
+    NgIf,
+    NgForOf
+  ],
+  styleUrls: ['./plp.component.scss']
+})
+export class PlpComponent implements OnInit {
+
+  plpList$: Observable<ProductPlpModel[]> = this._plpService.loadProducts() as Observable<ProductPlpModel[]>;
+
+  constructor(private _plpService: PlpService) { }
+
+  ngOnInit(): void { }
+}
